e341194ce9bcecae5bc6d944687c7bd98b32c30e
greg <lokkotarakun@gmail.com>
2023-11-14 00:36:27 +0100

    fix: disabling newsletter input while resolving issue with deploying strapi

--- src/pages/ComingSoon/ComingSoon.tsx
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -1,23 +1,24 @@
 import "./ComingSoon.scss";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
-import { AlertTitle, Stack, ThemeProvider, createTheme, styled } from "@mui/material";
+// import Box from "@mui/material/Box";
+// import TextField from "@mui/material/TextField";
+// import Button from "@mui/material/Button";
+// import SendIcon from "@mui/icons-material/Send";
+import { AlertTitle, Stack, ThemeProvider, createTheme } from "@mui/material";
+// import { AlertTitle, Stack, ThemeProvider, createTheme, styled } from "@mui/material"; a échanger avec la ligne du dessu s pour remettre
 import { COMINGSOON } from "../../assets/translation/french.json";
 import { useState } from "react";
-import { postEmailFetch } from "../../services/fetcher";
+// import { postEmailFetch } from "../../services/fetcher";
 import Alert from "@mui/material/Alert";
-import translations from "../../assets/strapi";
+// import translations from "../../assets/strapi";
 
 export default function ComingSoon() {
-	const CustomButton = styled(Button)({
-		fontFamily: ["Atkinson Hyperlegible"],
-		"&:hover": {
-			backgroundColor: "#191a19",
-			color: "#FCF2E3",
-		},
-	});
+	// const CustomButton = styled(Button)({
+	// 	fontFamily: ["Atkinson Hyperlegible"],
+	// 	"&:hover": {
+	// 		backgroundColor: "#191a19",
+	// 		color: "#FCF2E3",
+	// 	},
+	// });
 
 	const theme = createTheme({
 		palette: {
@@ -26,39 +27,39 @@ export default function ComingSoon() {
 		},
 	});
 
-	const [email, setEmail] = useState("");
+	// const [email, setEmail] = useState("");
 	const [errorMessage, setErrorMessage] = useState("");
 	const [successMessage, setSuccessMessage] = useState("");
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const handleChange = (event: { target: { value: any } }) => {
-		setEmail(event.target.value);
-	};
+	// const handleChange = (event: { target: { value: any } }) => {
+	// 	setEmail(event.target.value);
+	// };
 
-	async function handleClick() {
-		const apiToken = import.meta.env.VITE_REACT_APP_API_TOKEN;
-		const req = await postEmailFetch(
-			email,
-			apiToken
-		);
-		const isErrorResponse = req.error ? true : false;
-		if (isErrorResponse) {
-			setErrorMessage(translations[req.error.message]);
-			setSuccessMessage("");
+	// async function handleClick() {
+	// 	const apiToken = import.meta.env.VITE_REACT_APP_API_TOKEN;
+	// 	const req = await postEmailFetch(
+	// 		email,
+	// 		apiToken
+	// 	);
+	// 	const isErrorResponse = req.error ? true : false;
+	// 	if (isErrorResponse) {
+	// 		setErrorMessage(translations[req.error.message]);
+	// 		setSuccessMessage("");
 
-			setTimeout(() => {
-				setErrorMessage("");
-			}, 5000);
-		} else {
-			setErrorMessage("");
-			setSuccessMessage("Email envoyé avec succès !");
-			setEmail("");
+	// 		setTimeout(() => {
+	// 			setErrorMessage("");
+	// 		}, 5000);
+	// 	} else {
+	// 		setErrorMessage("");
+	// 		setSuccessMessage("Email envoyé avec succès !");
+	// 		setEmail("");
 
-			setTimeout(() => {
-				setSuccessMessage("");
-			}, 10000);
-		}
-	}
+	// 		setTimeout(() => {
+	// 			setSuccessMessage("");
+	// 		}, 10000);
+	// 	}
+	// }
 
 	return (
 		<ThemeProvider theme={theme}>
@@ -86,26 +87,6 @@ export default function ComingSoon() {
 					<p>{COMINGSOON.content}</p>
 				</div>
 				<div className="rightPartContainer"></div>
-				<div className="bottomPartContainer">
-					<Box className="newsletterForm" component="form">
-						<p>{COMINGSOON.label}</p>
-						<TextField
-							onChange={handleChange}
-							color="primary"
-							value={email}
-							size="small"
-							margin="dense"
-							fullWidth
-							id="emailNewsletter"
-							name="emailNewsletter"
-							label="Adresse mail"
-							variant="filled"
-						/>
-						<CustomButton onClick={handleClick} color="primary" variant="contained" endIcon={<SendIcon />}>
-							{COMINGSOON.button}
-						</CustomButton>
-					</Box>
-				</div>
 			</main>
 		</ThemeProvider>
 	);
