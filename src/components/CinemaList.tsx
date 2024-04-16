b5248bde576cef5a5636c92ee2864e44c3bdc974
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2024-04-17 00:13:28 +0200

    feat: :sparkles: ajout ducomposant CinemaCardResponse dans le CinemaList

--- src/components/CinemaList.tsx
+++ src/components/CinemaList.tsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { fetchDatas } from "../services/fetcher";
 import { CinemaObject } from "../Interfaces/Interfaces";
+import CinemaCardResponse from "./ChatBot/CinemaCardResponse/CinemaCardResponse";
 
 const CinemaList: React.FC = () => {
-  // Stocke la liste des cinémas dans un tableau state
   const [cinemas, setCinemas] = useState<CinemaObject[]>([]);
 
-  // useEffect va effectuer la requête fetch() lorsque le composant est monté
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchDatas("cinemas");
         setCinemas(data);
-        console.log("Cinemas", data);
       } catch (error) {
         console.error("Erreur lors de la récupération des cinémas", error);
       }
@@ -21,13 +19,11 @@ const CinemaList: React.FC = () => {
   }, []);
 
   return (
-    <ul>
+    <div>
       {cinemas.map((cinema) => (
-        <li key={cinema.id}>
-          {cinema.name} - {cinema.address1}
-        </li>
+        <CinemaCardResponse key={cinema.id} cinema={cinema} />
       ))}
-    </ul>
+    </div>
   );
 };
 
